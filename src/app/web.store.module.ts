deeaf51b0eaaffa027e8cb27021bef528d1ee43a
ppadalka <ppadalka@inbox.ru>
2017-01-22 19:35:09 +0200

    added router configuration (without defined routes)

--- src/app/web.store.module.ts
+++ src/app/web.store.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { WebStoreRoutingModule } from './web-store-routing.module';
+
 import { MainComponent } from './common/main.component';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        WebStoreRoutingModule
     ],
     declarations: [
         MainComponent
