d4c996aab8a188eb31b71e34c03c9dc23a4b079b
ppadalka <ppadalka@inbox.ru>
2017-02-12 18:58:51 +0200

    added angular reactive form to registration page

--- src/app/anonymous/registration/registration.component.ts
+++ src/app/anonymous/registration/registration.component.ts
@@ -1,6 +1,7 @@
-import {Component} from '@angular/core';
-import {FormGroup} from '@angular/forms';
-import {FormBuilder} from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { FormBuilder } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 import {RegistrationService} from './registration.service';
 
@@ -10,9 +11,23 @@ import {RegistrationService} from './registration.service';
 })
 export class RegistrationComponent {
 
-    profileForm: FormGroup;
-    accountForm: FormGroup;
+    registrationForm: FormGroup;
 
     constructor(private formBuilder: FormBuilder,
-                private registrationService: RegistrationService) {}
+                private registrationService: RegistrationService) {
+        this.createForm();
+    }
+
+    createForm() {
+        this.registrationForm = this.formBuilder.group({
+            username: ['', Validators.required],
+            email: ['', Validators.required],
+            password: ['', Validators.required],
+            confirmPassword: ['', Validators.required],
+            firstName: ['', Validators.required],
+            lastName: ['', Validators.required],
+            address: ['', Validators.required],
+            phoneNumber: ['', Validators.required]
+        });
+    }
 }
