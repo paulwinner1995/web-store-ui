5f3ee5149f01ee4acfe33bb05855d6a853cbfdd0
ppadalka <ppadalka@inbox.ru>
2017-02-25 21:17:05 +0200

    added displaying categories

--- src/app/sales/category/sales-category.component.ts
+++ src/app/sales/category/sales-category.component.ts
@@ -1,7 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CategoryService } from "../../common/category/category.service";
+import { Category } from "../../common/category/category";
 
 @Component({
     selector: 'ws-sales-category-component',
     templateUrl: './sales-category.component.html'
 })
-export class SalesCategoryComponent {}
+export class SalesCategoryComponent implements OnInit {
+
+    categories: Category[];
+
+    constructor(private categoryService: CategoryService) {}
+
+    ngOnInit(): void {
+        this.fetchCategories();
+    }
+
+    fetchCategories() {
+        this.categoryService.fetchCategories()
+            .subscribe(
+                categories => this.categories = categories,
+                error => console.log(error)
+            )
+    }
+}
