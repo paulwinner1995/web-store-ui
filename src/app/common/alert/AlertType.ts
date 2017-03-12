47f32d58fe69e168176e5daf92b377c336ef3faf
ppadalka <ppadalka@inbox.ru>
2017-03-12 19:25:35 +0200

    added class alert types

---
+++ src/app/common/alert/AlertType.ts
@@ -0,0 +1,6 @@
+export class AlertType {
+    public static SUCCESS: string = 'success';
+    public static INFO:    string = 'info';
+    public static WARNING: string = 'warning';
+    public static DANGER:  string = 'danger';
+}
