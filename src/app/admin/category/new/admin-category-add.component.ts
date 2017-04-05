05259f9f7e1385150364a4d56973ccaa626eb392
ppadalka <ppadalka@inbox.ru>
2017-04-05 23:29:45 +0300

    added form for saving category

--- src/app/admin/category/new/admin-category-add.component.ts
+++ src/app/admin/category/new/admin-category-add.component.ts
@@ -1,9 +1,28 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'ws-admin-category-add',
     templateUrl: './admin-category-add.component.html'
 })
-export class AdminCategoryAddComponent {
+export class AdminCategoryAddComponent implements OnInit {
 
+    categoryForm: FormGroup;
+
+    constructor(private formBuilder: FormBuilder) {}
+
+    ngOnInit(): void {
+        this.createForm();
+    }
+
+    createForm(): void {
+        this.categoryForm = this.formBuilder.group({
+            name: ['', Validators.required],
+            parent: ['']
+        });
+    }
+
+    onSubmit(): void {
+        alert("Category has been saved!");
+    }
 }
