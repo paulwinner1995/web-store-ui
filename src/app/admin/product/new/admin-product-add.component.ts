1e577d17356e2b5e1318f38875d428e62f2d05ee
PavelPadalka <ppadalka@inbox.ru>
2017-05-02 14:41:26 +0300

    Added some changes for product form

--- src/app/admin/product/new/admin-product-add.component.ts
+++ src/app/admin/product/new/admin-product-add.component.ts
@@ -1,8 +1,22 @@
 import { Component } from "@angular/core";
+import {Router} from "@angular/router";
+import {CategoryService} from "../../../common/category/category.service";
+import {Observable} from "rxjs/Observable";
+import {Product} from "../../../common/product/product";
 
 @Component({
     selector: 'ws-admin-product-add',
     templateUrl: './admin-product-add.component.html'
 })
 export class AdminProductAddComponent {
+
+    product: Product = new Product();
+
+    constructor(private router: Router, private categoryService: CategoryService) {}
+
+    categoryNames(example: string): Observable<string[]> {
+        return this.categoryService.fetchCategoryNames(example);
+    }
+
+    onSubmit() : void {}
 }
--- src/app/common/product/product.ts
+++ src/app/common/product/product.ts
@@ -3,5 +3,6 @@ export class Product {
     constructor(public name?: string,
                 public code?: string,
                 public amount?: number,
-                public price?: number) {}
+                public price?: number,
+                public category?: string) {}
 }
