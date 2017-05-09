55d79d8bb6b7ad3dfb2a4692d3e567b95fcf89fc
PavelPadalka <ppadalka@inbox.ru>
2017-05-09 19:39:15 +0300

    made catalog

--- src/app/common/catalog/catalog.component.ts
+++ src/app/common/catalog/catalog.component.ts
@@ -3,6 +3,7 @@ import { Page } from "../pagination/page";
 import { Product } from "../product/product";
 import { OnPageChange } from "../pagination/pageable";
 import { PageRequest } from "../pagination/page.request";
+import { ProductService } from "../product/product.service";
 
 @Component({
     selector: 'web-store-catalog',
@@ -12,9 +13,19 @@ export class CatalogComponent implements OnInit, OnPageChange {
 
     page: Page<Product>;
 
+    constructor(private productService: ProductService) {
+        this.page = new Page<Product>();
+    }
+
     ngOnInit(): void {
+        this.onPageChange(new PageRequest(0, 6));
     }
 
     onPageChange(pageRequest: PageRequest): void {
+        this.productService.fetchProducts(pageRequest)
+            .subscribe(
+                page => this.page = page,
+                error => console.log(error)
+            )
     }
 }
